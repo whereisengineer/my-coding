a51f069aa355a1ac169b8f63181b53b66d2f56b5
Gaurav Kalra <whereisengineer@hotmail.com>
2021-03-14 15:51:57 +0530

    Maximum equlibrium sum in an array and Equilibrium index of an array

--- Arrays/src/SearchingProb8.java
+++ Arrays/src/SearchingProb8.java
@@ -1,10 +1,11 @@
-//Maximum equlibrium sum in an array
+//Maximum equlibrium sum in an array and 
+// Equilibrium index of an array
 public class SearchingProb8 {
 	
 	public static void main(String[] arg)
 	{
-		int[] arr = {-1,0,3,1,3,4,-1};
-		int pref = 0, sum =0, res =0;
+		int[] arr = {-1,2,3,1,3,2,-1};
+		int pref = 0, sum =0, res =0, index =0;
 		
 		for(int i=0; i<arr.length; i++)
 			sum = sum + arr[i];
@@ -14,12 +15,17 @@ public class SearchingProb8 {
 			pref = pref + arr[i];
 			if(sum == pref)
 			{
-				res = Math.max(res, pref);
+				if(res < pref)
+				{
+					res = pref;
+					index = i;
+				}
 			}
 			sum = sum - arr[i];		
 		}
 		
 		System.out.println("max equilibrium sum is "+ res);
+		System.out.println("Equilibrium index is "+ index);
 	}
 
 }
