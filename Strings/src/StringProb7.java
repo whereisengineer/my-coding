9fc90cce26a9af84067c95eef668dea6411c9157
Gaurav Kalra <whereisengineer@hotmail.com>
2021-05-07 08:40:19 +0530

    Quick way to check if all the characters of a string are same

---
+++ Strings/src/StringProb7.java
@@ -0,0 +1,27 @@
+//Quick way to check if all the characters of a string are same
+public class StringProb7 {
+	
+	public static void main(String[] arg)
+	{
+		String str = "aaaaaa.";
+		String str2 = "Gaurav";
+		
+		checkChar(str);
+		checkChar(str2);
+	}
+	
+	public static void checkChar(String str)
+	{
+		for(int i =0 ; i<str.length()-1; i++)
+		{
+			if(str.charAt(i) != str.charAt(i+1))
+			{
+				System.out.println("Charaters not same!");
+				return;
+			}
+		}
+		
+		System.out.println("Characters same");
+	}
+
+}
